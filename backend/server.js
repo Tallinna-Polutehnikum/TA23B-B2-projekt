3dba9616cce6b459bec30c68054665e79420cae5
BogdanAnokhin <145449894+BogdanAnokhin@users.noreply.github.com>
2025-12-02 12:31:38 +0200

    merge main updates into prisma branch

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import express, { json } from 'express';
-const app = express()
-const port = 3000
-app.use(json());
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
